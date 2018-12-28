2b3a3ca4a7ff82bada85a46fac68eedb69f3461c
Stephen Bahr <sbahr@bu.edu>
2018-12-28 16:02:02 -0500

    - Adds Calculator, the main plugin class.

---
+++ src/main/java/me/sbahr/mc_calculator/Calculator.java
@@ -0,0 +1,18 @@
+package me.sbahr.mc_calculator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.sbahr.mc_calculator.listener.CalculatorMenu;
+
+public class Calculator extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		new CalculatorMenu(this);
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
